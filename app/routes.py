97da851190de4de7a9b4277a204d13887c490e65
Ayush Kumar Shah <ayush.kumar.shah@gmail.com>
2020-06-11 02:55:08 +0545

    Flask-wtf and security

--- app/routes.py
+++ app/routes.py
@@ -1,44 +1,44 @@
 from app import app, db
-from flask import render_template, request, Response, json
+from flask import render_template, request, Response, json, redirect, flash, url_for
 from app.models import User, Course, Enrollment
+from app.forms import LoginForm, RegisterForm
 
-courseData = [
-    {
-        "courseID": "1111",
-        "title": "PHP 101",
-        "description": "Intro to PHP",
-        "credits": 3,
-        "term": "Fall, Spring",
-    },
-    {
-        "courseID": "2222",
-        "title": "Java 1",
-        "description": "Intro to Java Programming",
-        "credits": 4,
-        "term": "Spring",
-    },
-    {
-        "courseID": "3333",
-        "title": "Adv PHP 201",
-        "description": "Advanced PHP Programming",
-        "credits": 3,
-        "term": "Fall",
-    },
-    {
-        "courseID": "4444",
-        "title": "Angular 1",
-        "description": "Intro to Angular",
-        "credits": 3,
-        "term": "Fall, Spring",
-    },
-    {
-        "courseID": "5555",
-        "title": "Java 2",
-        "description": "Advanced Java Programming",
-        "credits": 4,
-        "term": "Fall",
-    },
-]
+
+#         "courseID": "1111",
+#         "title": "PHP 101",
+#         "description": "Intro to PHP",
+#         "credits": 3,
+#         "term": "Fall, Spring",
+#     },
+#     {
+#         "courseID": "2222",
+#         "title": "Java 1",
+#         "description": "Intro to Java Programming",
+#         "credits": 4,
+#         "term": "Spring",
+#     },
+#     {
+#         "courseID": "3333",
+#         "title": "Adv PHP 201",
+#         "description": "Advanced PHP Programming",
+#         "credits": 3,
+#         "term": "Fall",
+#     },
+#     {
+#         "courseID": "4444",
+#         "title": "Angular 1",
+#         "description": "Intro to Angular",
+#         "credits": 3,
+#         "term": "Fall, Spring",
+#     },
+#     {
+#         "courseID": "5555",
+#         "title": "Java 2",
+#         "description": "Advanced Java Programming",
+#         "credits": 4,
+#         "term": "Fall",
+#     },
+# # ]
 
 
 @app.route("/")
@@ -50,20 +50,55 @@ def index():
 
 @app.route("/courses/")
 @app.route("/courses/<term>")
-def courses(term="2019"):
+def courses(term=None):
+    if term is None:
+        term = 2019
+    classes = Course.objects.order_by("-courseID")  # - for descending order
     return render_template(
-        "courses.html", courseData=courseData, courses=True, term=term
+        "courses.html", courseData=classes, courses=True, term=term
     )
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", login=True)
+    form = LoginForm()
+    if form.validate_on_submit():
+        # form data
+        email = form.email.data
+        password = form.password.data
+
+        # comapre form email with email in database
+        user = User.objects(email=email).first()
+        
+        if user and user.get_password(password):
+        # if request.form.get("email") == "test@uta.com":
+            flash("{}, You are successfully logged in".format(user.first_name), "success")
+            return redirect("/index")
+        else:
+            flash("Sorry, something went wrong", "danger")
+
+    return render_template("login.html", title="Login", form=form, login=True)
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html", register=True)
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_id = 20 + User.objects.count()
+        user_id += 1
+
+        email = form.email.data
+        password = form.password.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        user = User(user_id=user_id, email=email, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save()
+
+        flash("You have registered successfully", "success")
+        return redirect(url_for('index'))
+    return render_template("register.html", title="Register", form=form, register=True)
 
 
 @app.route("/enrollment", methods=["GET", "POST"])
