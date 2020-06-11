9414464332ad390642f33a7dd3cc8ca16c95aeb0
Ayush Kumar Shah <ayush.kumar.shah@gmail.com>
2020-06-12 02:05:25 +0545

    Add 4 REST APIs

--- app/routes.py
+++ app/routes.py
@@ -1,44 +1,49 @@
-from app import app, db
-from flask import render_template, request, Response, json, redirect, flash, url_for, session
+from app import app, db, api
+from flask import render_template, request, Response, json, jsonify, redirect, flash, url_for, session
 from app.models import User, Course, Enrollment
 from app.forms import LoginForm, RegisterForm
+from flask_restplus import Resource
+from app.course_list import course_list
+
+#####################################################
+
+@api.route('/api', '/api/')
+class GetAndPost(Resource):
+
+    # GET ALL
+    def get(self):
+        return jsonify(User.objects.all())
 
 
-#         "courseID": "1111",
-#         "title": "PHP 101",
-#         "description": "Intro to PHP",
-#         "credits": 3,
-#         "term": "Fall, Spring",
-#     },
-#     {
-#         "courseID": "2222",
-#         "title": "Java 1",
-#         "description": "Intro to Java Programming",
-#         "credits": 4,
-#         "term": "Spring",
-#     },
-#     {
-#         "courseID": "3333",
-#         "title": "Adv PHP 201",
-#         "description": "Advanced PHP Programming",
-#         "credits": 3,
-#         "term": "Fall",
-#     },
-#     {
-#         "courseID": "4444",
-#         "title": "Angular 1",
-#         "description": "Intro to Angular",
-#         "credits": 3,
-#         "term": "Fall, Spring",
-#     },
-#     {
-#         "courseID": "5555",
-#         "title": "Java 2",
-#         "description": "Advanced Java Programming",
-#         "credits": 4,
-#         "term": "Fall",
-#     },
-# # ]
+    # POST
+    def post(self):
+       
+        data = api.payload
+
+        user = User(user_id=data['user_id'], email=data['email'], 
+                    first_name=data['first_name'], last_name=data['last_name'])
+        user.set_password(data['password'])
+        user.save()
+        return jsonify(User.objects(user_id=data['user_id']))
+
+@api.route('/api', '/api/<idx>')
+class GetUpdateDelete(Resource):
+
+    # GET One
+    def get(self, idx):
+        return jsonify(User.objects(user_id=idx))
+
+    # PUT
+    def put(self, idx):
+        data = api.payload
+        User.objects(user_id=idx).update(**data)
+        return jsonify(User.objects(user_id=idx))
+    
+    # DELETE
+    def delete(self, idx):
+        User.objects(user_id=idx).delete()
+        return jsonify("User is deleted")
+###################################################
 
 
 @app.route("/")
@@ -131,58 +136,22 @@ def enrollment():
         else:
             Enrollment(user_id=user_id, courseID=courseID).save()
             flash("You are enrolled in {}".format(courseTitle), "success")
-
-    classes = list(User.objects.aggregate(*[
-                                            {
-                                                '$lookup': {
-                                                    'from': 'enrollment', 
-                                                    'localField': 'user_id', 
-                                                    'foreignField': 'user_id', 
-                                                    'as': 'r1'
-                                                }
-                                            }, {
-                                                '$unwind': {
-                                                    'path': '$r1', 
-                                                    'includeArrayIndex': 'r1_id', 
-                                                    'preserveNullAndEmptyArrays': False
-                                                }
-                                            }, {
-                                                '$lookup': {
-                                                    'from': 'course', 
-                                                    'localField': 'r1.courseID', 
-                                                    'foreignField': 'courseID', 
-                                                    'as': 'r2'
-                                                }
-                                            }, {
-                                                '$unwind': {
-                                                    'path': '$r2', 
-                                                    'preserveNullAndEmptyArrays': False
-                                                }
-                                            }, {
-                                                '$match': {
-                                                    'user_id': user_id 
-                                                }
-                                            }, {
-                                                '$sort': {
-                                                    'courseID': 1
-                                                }
-                                            }
-                                        ]))
-
+    
+    classes = course_list()
     return render_template(
         "enrollment.html", enrollment=True, title="Enrollment", classes=classes
     )
 
 
-@app.route("/api/")
-@app.route("/api/<idx>")
-def api(idx=None):
-    if not idx:
-        jdata = courseData
-    else:
-        jdata = courseData[int(idx)]
+# @app.route("/api/")
+# @app.route("/api/<idx>")
+# def api(idx=None):
+#     if not idx:
+#         jdata = courseData
+#     else:
+#         jdata = courseData[int(idx)]
 
-    return Response(json.dumps(jdata), mimetype="application/json")
+#     # return Response(json.dumps(jdata), mimetype="application/json")
 
 
 @app.route("/user")
--- docs/flask/rest-api.md
+++ docs/flask/rest-api.md
@@ -24,3 +24,95 @@ $ pip install flask-restplus
 - API Monitoring
 
 
+
+## 3. Create API
+
+|Request|Operation|Description|
+|---|---|---|
+|POST|CREATE|Insert data|
+|GET|READ|Fetch data|
+|PUT|UPDATE|Replace Existing data|
+|DELETE|Delete|Remove data|
+
+### Fetch data using GET
+
+#### Add Api library in `__init__.py`
+
+```python
+from flask_restplus import api
+
+api = api()
+app = flask(__name__)
+app.config.from_object(config)
+
+db = mongoengine()
+db.init_app(app)
+
+api.init_app(app)
+
+from app import routes
+```
+
+#### Create API in routes
+
+`routes.py`
+
+```python
+from app import Api
+from flask_restplus import Resource
+
+#####################################################
+
+@api.route('/api', '/api/')
+class GetAndPost(Resource):
+
+    def get(self):
+        return jsonify(User.objects.all())
+
+
+
+@api.route('/api', '/api/<idx>')
+class GetUpdateDelete(Resource):
+
+    def get(self, idx):
+        return jsonify(User.objects(user_id=idx))
+
+###################################################
+
+```
+
+### Write data using POST
+
+```python
+# POST
+def post(self):
+   
+    data = api.payload
+
+    user = User(user_id=data['user_id'], email=data['email'], 
+                first_name=data['first_name'], last_name=data['last_name'])
+    user.set_password(data['password'])
+    user.save()
+    return jsonify(User.objects(user_id=data['user_id']))
+```
+
+Add JSON data in postman using post request
+
+### UPdate data using PUT
+
+```python
+# PUT
+def put(self, idx):
+    data = api.payload
+    User.objects(user_id=idx).update(**data)
+    return jsonify(User.objects(user_id=idx))
+```
+
+### Delete data using DELETE
+
+```python
+# DELETE
+def delete(self, idx):
+    User.objects(user_id=idx).delete()
+    return jsonify("User is deleted")
+```
